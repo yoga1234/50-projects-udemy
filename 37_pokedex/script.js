3cb2a82fb70a7b318eed7949cd7e6bc55460f269
Septiana Yoga <septianayoga30@gmail.com>
2022-01-14 16:34:19 +0700

    finishing pokedex app

--- 37_pokedex/script.js
+++ 37_pokedex/script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById("poke-container");
-const pokemont_count = 150;
+const pokemon_count = 150;
 const colors = {
   fire: "#FDDFDF",
   grass: "#DEFDE0",
@@ -16,6 +16,7 @@ const colors = {
   fighting: "#E6E0D4",
   normal: "#F5F5F5",
 };
+const main_types = Object.keys(colors);
 
 const fetchPokemons = async () => {
   for (let i = 1; i < pokemon_count; i++) {
@@ -27,5 +28,34 @@ const getPokemon = async (id) => {
   const res = await fetch(url);
   const data = await res.json();
 
+  createPokemonCard(data);
 };
 
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, "0");
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+    <div class="img-container">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}" >
+    </div>
+    <div class="info">
+      <span class="number">#${id}</span>
+      <h3 class="name">${name}</h3>
+      <small class="type">Type: <span>${type}</span></small>
+    </div>
+  `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  poke_container.appendChild(pokemonEl);
+};
+
+fetchPokemons();
